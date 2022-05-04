5cbe6f61872cc8e69eb814284f9667f3516f4c2f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-05-04 07:03:29 -0300

    Update 5_deploy_factory.js

--- scripts/5_deploy_factory.js
+++ scripts/5_deploy_factory.js
@@ -7,7 +7,7 @@ const {
 } = require('./constants');
 
 async function main() {
-  const Factory = await ethers.getContractFactory('FantomNFTFactory');
+  const Factory = await ethers.getContractFactory('RSKNFTFactory');
   const factory = await Factory.deploy(
     AUCTION,
     MARKETPLACE,
@@ -17,10 +17,10 @@ async function main() {
     '50000000000000000000'
   );
   await factory.deployed();
-  console.log('FantomNFTFactory deployed to:', factory.address);
+  console.log('RSKNFTFactory deployed to:', factory.address);
 
   const PrivateFactory = await ethers.getContractFactory(
-    'FantomNFTFactoryPrivate'
+    'RSKNFTFactoryPrivate'
   );
   const privateFactory = await PrivateFactory.deploy(
     AUCTION,
@@ -31,7 +31,7 @@ async function main() {
     '50000000000000000000'
   );
   await privateFactory.deployed();
-  console.log('FantomNFTFactoryPrivate deployed to:', privateFactory.address);
+  console.log('RSKNFTFactoryPrivate deployed to:', privateFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
